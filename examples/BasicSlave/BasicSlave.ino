b26fa9a955088056e51a52dfc3ddbabdd1d973c7
turmary <turmary@126.com>
2020-08-04 15:20:01 +0800

    Add: Basic Slave example

---
+++ examples/BasicSlave/BasicSlave.ino
@@ -0,0 +1,76 @@
+/*
+ * Name: BasicSlave.ino
+ *   Basic SPI Slave demo.
+ *
+ * Author: turmary <turmary@126.com>
+ * Copyright (c) 2020 Seeed Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <stdio.h>
+#include <SPISlave.h>
+
+#define _SER_DEBUG 0
+
+/**************************************************************/
+void requestEvent(int freeSpace);
+void receiveEvent(int howMany);
+
+/***************************************************************
+ * states debug function                                       *
+ **************************************************************/
+int debug(int v) {
+	#if _SER_DEBUG
+	Serial.println(v);
+	#endif
+	return 0;
+}
+
+void setup()
+{
+	#if _SER_DEBUG
+	Serial.begin(115200);
+	#endif
+
+	debug(0);
+
+	SPISlave.begin();
+	SPISlave.onReceive(receiveEvent);
+	SPISlave.onRequest(requestEvent);
+
+	debug(1);
+	return;
+}
+
+void loop()
+{
+}
+
+void receiveEvent(int howMany)
+{
+	(void)howMany;
+	return;
+}
+
+void requestEvent(int freeSpace)
+{
+	(void)freeSpace;
+	return;
+}
